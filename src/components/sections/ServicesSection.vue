963fe00fc853a0b658f2c5d46b0cb00f75e531e5
Valera Kalinichenko <studiya3@gmail.com>
2023-12-13 11:30:37 +0200

    services section impl

--- src/components/sections/ServicesSection.vue
+++ src/components/sections/ServicesSection.vue
@@ -1,3 +1,18 @@
 <template>
-  <section class="services"></section>
+  <section class="services">
+    <div class="wrapper">
+      <div class="services__inner">
+        <h2 class="services__title"></h2>
+        <p class="services__subtitle"></p>
+        <ul class="services__list">
+          <li class="services__item">
+            <h3 class="services__item-title"></h3>
+            <p class="services__item-subtitle"></p>
+            <button class="services__item-btn btn btn-grey" type="button"></button>
+            <img src="" alt="" class="services__item-img" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
 </template>
