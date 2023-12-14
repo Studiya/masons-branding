8c7a887cc088f24a252219780768937cc0dccc72
Valera Kalinichenko <studiya3@gmail.com>
2023-12-14 09:56:04 +0200

    examples section impl

--- src/components/sections/ExamplesSection.vue
+++ src/components/sections/ExamplesSection.vue
@@ -1,3 +1,102 @@
 <template>
-  <section class="examples"></section>
+  <section class="examples">
+    <div class="wrapper">
+      <div class="examples__inner">
+        <h3 class="examples__title"><span class="accent">Exam</span>ples</h3>
+        <p class="examples__subtitle">You can wear it</p>
+        <Carousel class="examples__carousel" v-bind="settings">
+          <Slide class="examples__slide" v-for="item in examplesList" :key="item.id">
+            <a :href="item.href"><img :src="item.imgSrc" :alt="item.imgAlt" /></a
+          ></Slide>
+          <template #addons>
+            <Navigation />
+          </template>
+        </Carousel>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script setup>
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Navigation } from 'vue3-carousel'
+
+const settings = {
+  itemsToShow: 3,
+  itemsToScroll: 3,
+  snapAlign: 'center'
+}
+
+const examplesList = [
+  {
+    id: 0,
+    href: '#',
+    imgSrc: 'src/assets/images/examples-section/item.png',
+    imgAlt: 't-shirt'
+  },
+  {
+    id: 1,
+    href: '#',
+    imgSrc: 'src/assets/images/examples-section/item.png',
+    imgAlt: 't-shirt'
+  },
+  {
+    id: 2,
+    href: '#',
+    imgSrc: 'src/assets/images/examples-section/item.png',
+    imgAlt: 't-shirt'
+  },
+  {
+    id: 3,
+    href: '#',
+    imgSrc: 'src/assets/images/examples-section/item.png',
+    imgAlt: 't-shirt'
+  },
+  {
+    id: 4,
+    href: '#',
+    imgSrc: 'src/assets/images/examples-section/item.png',
+    imgAlt: 't-shirt'
+  },
+  {
+    id: 5,
+    href: '#',
+    imgSrc: 'src/assets/images/examples-section/item.png',
+    imgAlt: 't-shirt'
+  },
+  {
+    id: 6,
+    href: '#',
+    imgSrc: 'src/assets/images/examples-section/item.png',
+    imgAlt: 't-shirt'
+  }
+]
+</script>
+
+<style lang="scss" scoped>
+.examples {
+  margin-bottom: 150px;
+
+  .wrapper {
+    max-width: 1440px;
+  }
+
+  &__inner {
+    max-width: 1056px;
+    margin: 0 auto;
+  }
+
+  &__subtitle {
+    color: var(--color-grey);
+    font-size: 32px;
+    font-weight: 300;
+    margin-bottom: 30px;
+  }
+
+  &__slide {
+    a {
+      max-width: 272px;
+    }
+  }
+}
+</style>
